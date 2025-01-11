305d6dbd9375c307d6b39302197e12b7577031be
andriitopii <andriitopii@icloud.com>
2025-01-11 16:11:08 +0100

    Asdasd

--- src/bd/firebase.js
+++ src/bd/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: "herhelclinic-710b9.firebaseapp.com",
-  projectId: "herhelclinic-710b9",
-  storageBucket: "herhelclinic-710b9.appspot.com",
-  messagingSenderId: "326121729425",
-  appId: "1:326121729425:web:b8540856896c7d5cf53e30"
+  authDomain: "herhelbilobrov.firebaseapp.com",
+  projectId: "herhelbilobrov",
+  storageBucket: "herhelbilobrov.firebasestorage.app",
+  messagingSenderId: "541100728159",
+  appId: "1:541100728159:web:1c4076e0b642d23fa4e0ea"
 };
 
 // Initialize Firebase
